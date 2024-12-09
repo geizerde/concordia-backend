e502dda0c14b6bdb95f939ea03fc1f3ed5f364d1
geizerde <geizerde@gmail.com>
2024-12-09 15:28:57 +0300

    CN-B-9: change the CountDTO int type to Long

--- src/main/java/ru/sirius/concordia/chat/repository/ChatMessageRepository.java
+++ src/main/java/ru/sirius/concordia/chat/repository/ChatMessageRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 
-    int countBySenderIdAndRecipientIdAndStatus(
+    Long countBySenderIdAndRecipientIdAndStatus(
             String senderId,
             String recipientId,
             MessageStatus status
